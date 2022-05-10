911fc8e8ac25d539cdcf994a96b4c635e6980b19
Rupali Natewad <rupalirnatewad@gmail.com>
2022-05-10 15:37:49 +0530

    Add project files.

---
+++ GenericsDemo/Demo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    internal class Demo
+    {
+        
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+
+        public static void toPrint(int[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+
+        public static void toPrint(double[] inputArray)
+        {
+
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+
+        public static void toPrint(char[] inputArray)
+        {
+            foreach (char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------------------------------");
+        }
+
+    }
+}
---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericsDemo;
+Console.WriteLine("Hello, World!");
+int[] intArray = { 1, 2, 3, 4, 5 };
+double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+//Program.toPrint(intArray);
+//Program.toPrint(doubleArray);
+//Program.toPrint(charArray);
+
+Demo.toPrint<int>(intArray);
+Demo.toPrint<double>(doubleArray);
+Demo.toPrint<char>(charArray);
+Console.ReadKey();
+
+
